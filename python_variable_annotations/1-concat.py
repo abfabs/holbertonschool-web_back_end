ec211b379504169b132f7baa7004e58e6303fa1b
abfabs <albaeftimi@gmail.com>
2025-11-25 11:12:33 +0100

    1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Module 1-concat: type-annotated function concat that concatenates two strings."""
 
-
 def concat(str1: str, str2: str) -> str:
     """Return the concatenation of str1 and str2."""
     return str1 + str2
