c50a9cbdf9541f03a16074af6866826a0fa111df
abfabs <albaeftimi@gmail.com>
2025-11-25 10:52:58 +0100

    a couple of annotations

---
+++ 8-make_multiplier.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiplier_func(n: float) -> float:
+        return n * multiplier
+    return multiplier_func
