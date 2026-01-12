8d5d18f6c9f9f123c4902d579f37fb8bcd9916a5
abfabs <albaeftimi@gmail.com>
2026-01-12 12:09:03 +0100

    hi

--- python_variable_annotations/3-main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Module 3-to_str: type-annotated function to_str that converts a float to a string."""
-
-
-def to_str(n: float) -> str:
-    """Return the string representation of the float n."""
-    return str(n)
